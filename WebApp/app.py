d12428c5f4dc8df248a89fa299858f5b6fea0de3
LHKode <hoangkhanh9119@gmail.com>
2023-11-20 17:18:39 +0900

    update ngrok to forward public

--- WebApp/app.py
+++ WebApp/app.py
@@ -5,19 +5,19 @@ from ultralytics import YOLO
 st.title("NAME YOUR FOOD")
 col1, col2 = st.columns(2)
 
-model = YOLO('E:/LHK/Study/Subjects/MultiMedia/FoodRecognition/runs/detect/train41/weights/best.pt')
+model = YOLO('runs/detect/train41/weights/best.pt')
 
 file = st.file_uploader('Upload image', type=["jpg", "jpeg", "png", "bmp", "webp"])
 
-with col1:
-    uploaded_image = PIL.Image.open(file)
-    st.image(file, caption="Uploaded Image", use_column_width=True)
+if not (file is None):
+    with col1:
+        uploaded_image = PIL.Image.open(file)
+        st.image(file, caption="Uploaded Image", use_column_width=True)
 
-    if not (file is None):
         results = model.predict(uploaded_image)
-with col2:
-    boxes = results[0].boxes
-    res_plotted = results[0].plot()[:, :, ::-1]
-    st.image(res_plotted, caption='Detected Image',
-                use_column_width=True)
+        
+    with col2:
+        boxes = results[0].boxes
+        res_plotted = results[0].plot()[:, :, ::-1]
+        st.image(res_plotted, caption='Detected Image', use_column_width=True)
 
